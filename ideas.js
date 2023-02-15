00d9f1c27ee6638745bd953863bc996148b81f13
melinamui <113132371+melinamui@users.noreply.github.com>
2023-02-15 13:46:26 -0500

    Create ideas.js

---
+++ ideas.js
@@ -0,0 +1,25 @@
+//rip of game stop
+// call it Pink GameGo
+// everything that's pink
+//make my own array
+//add the total to the shop
+
+// do an calender/to do list maybe
+// use an algorithm to count the amount of tasks
+// array of all the months
+// they can switch between months
+// maybe array of all the holidays
+// holiday tab
+// let them add birthdays
+// birthday tab
+// https://javascript.info/task/calendar-table#:~:text=Write%20a%20function%20createCalendar(elem,and%20so%20on%20till%20Sunday.
+//https://www.youtube.com/watch?v=YhgoB1RhSLs
+
+// OR TO MAKE IT MORE SIMPLE
+// Have one with just the calender (use buttons)
+// have the date on the top
+// they can insert their own tasks
+// kinda like the class elite project
+
+// If I have time, in the beginning they could add their name
+// maybe they can change themes, IF I HAVE TIMe
